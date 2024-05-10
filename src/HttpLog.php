1e23aaf2ceacd6be9cbc0f70ac7cdc6b2708fe0c
wangc <wangchuang@hzwochi.com>
2024-05-10 14:43:49 +0800

    修改引入的类

--- src/HttpLog.php
+++ src/HttpLog.php
@@ -5,9 +5,11 @@
  * @datetime: 2024/5/9 18:01
  */
 namespace wangchuang91;
+
+use think\Response;
 class HttpLog
 {
-    public function handle($response)
+    public function handle(Response $response)
     {
         // 记录请求日志
         app()->log->record($response->getData());
